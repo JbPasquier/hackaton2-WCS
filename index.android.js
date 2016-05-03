8953ecfaccac7a1b984df09022934b9a4c82a5e2
Jean-Baptiste <contact@jbpasquier.eu>
2016-05-03 10:53:39 +0200

    BG Color

--- index.android.js
+++ index.android.js
@@ -5,21 +5,20 @@
 /*jshint esversion: 6*/
 'use strict';
 var React = require('react-native');
+var styles = React.StyleSheet.create({
+    parent: {
+        backgroundColor: '#26569e'
+    }
+});
 var Hackaton2 = React.createClass({
     render: function() {
-       var layout =
-           <React.View >
-
-               <React.Text>
-                   Bonjour
-               </React.Text>
-
-           </React.View>
-       ;
-       return layout;
-   }
-});
-var styles = React.StyleSheet.create({
-
+        var layout =
+        <React.View style = {styles.background} >
+            <React.Text >
+                Bonjour
+            </React.Text>
+        </React.View>;
+        return layout;
+    }
 });
 React.AppRegistry.registerComponent('hackaton2', () => Hackaton2);
