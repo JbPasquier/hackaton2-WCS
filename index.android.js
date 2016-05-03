30b703d239ad7387b03b566faf4abd2af3db4147
Jean-Baptiste <contact@jbpasquier.eu>
2016-05-03 10:17:49 +0200

    Add react-native-icons

--- index.android.js
+++ index.android.js
@@ -2,6 +2,7 @@
  * Sample React Native App
  * https://github.com/facebook/react-native
  */
+/*jshint esversion: 6*/
 'use strict';
 var React = require('react-native');
 var Hackaton2 = React.createClass({
