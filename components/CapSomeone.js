3f33dc56a899c44f54f58f053490deaad667f457
Jean-Baptiste <contact@jbpasquier.eu>
2016-05-04 03:42:04 +0200

    Welcome + CapSomeOne

--- components/CapSomeone.js
+++ components/CapSomeone.js
@@ -5,11 +5,48 @@ import React, {
     Text,
     Component,
     ScrollView,
+    StyleSheet,
+    Image,
+    TextInput,
     View
 } from 'react-native';
 import {
-    Subheader
+    Subheader,
+    Button,
+    Avatar
 } from 'react-native-material-design';
+const styles = StyleSheet.create({
+    main: {
+        backgroundColor: '#26569e',
+        flex: 1,
+        alignItems:'center',
+        paddingTop: 10
+    },
+    text: {
+        marginTop: 20,
+        color: 'white',
+        fontSize: 20
+    },
+    bold: {
+        fontWeight: "900"
+    },
+    logMeIn: {
+        flex:1,
+        backgroundColor: '#ff0000',
+        height: 100
+    },
+    avatarContainer: {
+        marginTop:40
+    },
+    small: {
+        fontSize: 14,
+        color: 'white'
+    },
+    gimmeAnHRBaby: {
+        borderBottomColor: "black",
+        borderBottomWidth: 1
+    }
+});
 class CapSomeone extends Component{
     constructor(props) {
         super(props)
@@ -31,8 +68,21 @@ class CapSomeone extends Component{
     }
   render() {
     return (
-        <ScrollView>
+        <ScrollView style = {{backgroundColor: '#26569e'}}>
             <Subheader text={"CapSomeone Screen"} />
+            <View style = {[styles.main, styles.gimmeAnHRBaby]}>
+                <View style = {[styles.main,{flexDirection:'row',flexWrap:'nowrap'}]}>
+                    <Avatar style={{justifyContent: 'flex-start'}} size={80} image={<Image source={require('../assets/thomas.jpg')}></Image>} />
+                    <Text style={{justifyContent: 'flex-end',marginLeft:40,marginRight:100}}>Défier Thomas</Text>
+                </View>
+                <Text>Défie les capt`Ûeurs locaux</Text>
+                <Text style = {styles.main}>Gagne des points et c'est cool</Text>
+            </View>
+            <View style = {styles.main}>
+                <Text style = {styles.main}>Quel sera son défi ?</Text>
+                <TextInput style = {[styles.main,{height: 150, backgroundColor:"#ffffff"}]} multiline = {true} placeholder={"Quel sera son défi ...?"} />
+                <Button style={{marginLeft:50}} raised = {true} primary = {"paperDeepOrange"} text = {"Défier"} theme = {"dark"} onPress={this.navWelcome.bind(this)} />
+            </View>
             <View>
                 <TouchableHighlight onPress={this.navCapMe.bind(this)}>
                     <Text>Navigate to capme screen</Text>
--- components/Welcome.js
+++ components/Welcome.js
@@ -5,11 +5,47 @@ import React, {
     Text,
     Component,
     ScrollView,
+    StyleSheet,
+    Image,
     View
 } from 'react-native';
 import {
-    Subheader
+    Subheader,
+    Button,
+    Avatar
 } from 'react-native-material-design';
+const styles = StyleSheet.create({
+    main: {
+        backgroundColor: '#26569e',
+        flex: 1,
+        alignItems:'center',
+        paddingTop: 10
+    },
+    text: {
+        marginTop: 20,
+        color: 'white',
+        fontSize: 20
+    },
+    bold: {
+        fontWeight: "900"
+    },
+    logMeIn: {
+        flex:1,
+        backgroundColor: '#ff0000',
+        height: 100
+    },
+    avatarContainer: {
+        marginTop:40
+    },
+    small: {
+        fontSize: 14,
+        color: 'white'
+    },
+    gimmeAnHRBaby: {
+        borderBottomColor: "black",
+        borderBottomWidth: 1
+    }
+});
 class Welcome extends Component{
     constructor(props) {
         super(props)
@@ -31,9 +67,45 @@ class Welcome extends Component{
     }
   render() {
     return (
-        <ScrollView>
-            <Subheader text={"Welcome Screen"} />
-            <View>
+        <ScrollView style = {{backgroundColor: '#26569e'}}>
+            <View style = {styles.main} />
+            <View style = {styles.main}>
+                <Image source={require('../assets/logo.png')}></Image>
+            </View>
+            <View style = {[styles.main, styles.gimmeAnHRBaby]}>
+                <Subheader text={"Bienvenue sur capt`Û"} />
+            </View>
+            <View style = {[styles.main, styles.gimmeAnHRBaby]}>
+                <View style = {[styles.main,{flexDirection:'row',flexWrap:'nowrap'}]}>
+                    <Avatar style={{justifyContent: 'flex-start'}} size={80} image={<Image source={require('../assets/jeremy.jpg')}></Image>} />
+                    <Text style={{justifyContent: 'flex-end',marginLeft:40,marginRight:100}}>Hello Jeremy</Text>
+                </View>
+                <Text>Défie les capt`Ûeurs locaux</Text>
+                <Text>Gagne des points et c'est cool</Text>
+            </View>
+            <View style = {[styles.main, styles.gimmeAnHRBaby]}>
+                <View style = {[styles.main,{flexDirection:'row',flexWrap:'nowrap'}]}>
+                    <Avatar style={{justifyContent: 'flex-start'}} size={40} image={<Image source={require('../assets/iban.jpg')}></Image>} />
+                    <Text style={{justifyContent: 'flex-end',marginLeft:20,marginRight:50}}>Iban{"\n"}2m - La Loupe</Text>
+                    <Button style={{justifyContent: 'flex-end',marginLeft:50}} raised = {true} primary = {"paperDeepOrange"} text = {"capt`Ŷ"} theme = {"dark"} onPress={this.navCapSomeone.bind(this)} />
+                </View>
+                <View style = {[styles.main,{flexDirection:'row',flexWrap:'nowrap'}]}>
+                    <Avatar style={{justifyContent: 'flex-start'}} size={40} image={<Image source={require('../assets/mauricio.jpg')}></Image>} />
+                    <Text style={{justifyContent: 'flex-end',marginLeft:20,marginRight:50}}>Mauricio{"\n"}4m - La Loupe</Text>
+                    <Button style={{justifyContent: 'flex-end',marginLeft:50}} raised = {true} primary = {"paperDeepOrange"} text = {"capt`Ŷ"} theme = {"dark"} onPress={this.navCapSomeone.bind(this)} />
+                </View>
+                <View style = {[styles.main,{flexDirection:'row',flexWrap:'nowrap'}]}>
+                    <Avatar style={{justifyContent: 'flex-start'}} size={40} image={<Image source={require('../assets/jb.jpg')}></Image>} />
+                    <Text style={{justifyContent: 'flex-end',marginLeft:20,marginRight:50}}>Jean-Baptiste{"\n"}8m - La Loupe</Text>
+                    <Button style={{justifyContent: 'flex-end',marginLeft:50}} raised = {true} primary = {"paperDeepOrange"} text = {"capt`Ŷ"} theme = {"dark"} onPress={this.navCapSomeone.bind(this)} />
+                </View>
+                <View style = {[styles.main,{flexDirection:'row',flexWrap:'nowrap'}]}>
+                    <Avatar style={{justifyContent: 'flex-start'}} size={40} image={<Image source={require('../assets/thomas.jpg')}></Image>} />
+                    <Text style={{justifyContent: 'flex-end',marginLeft:20,marginRight:30}}>Thomas{"\n"}384000 km - Lune</Text>
+                    <Button style={{justifyContent: 'flex-end',marginLeft:50}} raised = {true} primary = {"paperDeepOrange"} text = {"capt`Ŷ"} theme = {"dark"} onPress={this.navCapSomeone.bind(this)} />
+                </View>
+            </View>
+            <View style = {styles.main}>
                 <TouchableHighlight onPress={this.navCapMe.bind(this)}>
                     <Text>Navigate to capme screen</Text>
                 </TouchableHighlight>
